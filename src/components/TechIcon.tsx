de5308c391ed0c484af5fcff39352aac6432fe75
Mrunali1004 <zalavadiyamrunali@gmail.com>
2025-01-27 13:47:34 -0500

    Add About Section And Add icons

--- src/components/TechIcon.tsx
+++ src/components/TechIcon.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const TechIcon = () => {
+const TechIcon = ({ component }: { component: React.ElementType }) => {
+  const Component = component;
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Component className="size-10 fill-[url(#tech-icon-gradient)]" />
+      <svg className="size-0 absolute">
+        <linearGradient id="tech-icon-gradient">
+          <stop offset="0%" stopColor="rgb(52 211 153)" />
+          <stop offset="100%" stopColor="rgb(167 243 208)" />
+        </linearGradient>
+      </svg>
+    </>
+  );
+};
 
-export default TechIcon
+export default TechIcon;
